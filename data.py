7373b8de3e8ecff969a858bcb77ec7d239244b56
Paulina Espejo <pdespejo18@gmail.com>
2022-11-17 14:17:42 -0800

    blue lower range decreased

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
 # constants
 # colors in hsv dict for masks. the first value represents the lower limit and the second the lower
 HSV_COLORS = {
-    'blue': [[90,60,0], [121,255,255]], 
+    'blue': [[80,50,0], [121,255,255]], 
     'green': [[50, 100,100], [70,255,255]],
     'yellow': [[20, 100, 100], [30, 255, 255]],
     'black': [[0, 0, 0], [180,255,40]],
